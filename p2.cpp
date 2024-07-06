98db84ca25c021a65bb1c30123c6d035b985b438
di3go <djramosm@unjbg.edu.pe>
2024-07-05 22:51:14 -0500

    Se agregò la funciòn void menu() la cual despliega el menú de opciones a realizar

--- p2.cpp
+++ p2.cpp
@@ -14,6 +14,60 @@ struct Venta {
 };
 
 
+void menu(){
+    char opcion;
+    //vector<contactoEmail> contactos;
+    do {   
+        cout << "Seleccione la acción que desea realizar" << endl;
+        cout << "A. Registrar un nuevo producto" << endl;
+        cout << "B. Listar los productos registrados" << endl;
+        cout << "C. Buscar un producto por su nombre" << endl;
+        cout << "D. Actualizar los datos de un producto" << endl;
+        cout << "E. Eliminar un producto" << endl;
+        cout << "F. Registrar una venta" << endl;
+        cout << "G. Listar las ventas realizadas" << endl;
+        cout << "H. Calcular el total de ventas realizadas" << endl;
+        cout << "S. Salir" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+        cout << endl;
+        switch(opcion){
+            case 'S':
+                cout << "Saliendo del programa..." << endl;
+                break;
+            case 'A':
+                
+                break;
+            case 'B':
+                
+                break;
+            case 'C':
+                
+                break;
+            case 'D':
+                
+                break;
+            case 'E':
+                
+                break;
+            case 'F':
+                
+                break;
+            case 'G':
+                
+                break;
+            case 'H':
+                
+                break;
+            default:
+                cout << "Opción no válida";
+                break;
+        } 
+        cout << endl; 
+    } while (opcion != 'S');
+}
+
+
 
 int main(){
 
