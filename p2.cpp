e51cc66b30a13b60fe04b5497b71637ec4084fd7
di3go <djramosm@unjbg.edu.pe>
2024-07-05 22:42:33 -0500

    Se creó el segundo ejercicio y se agregaron las dos estructuras

---
+++ p2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct Producto {
+    string nombre;
+    float precio;
+};
+
+struct Venta {
+    int idProducto;
+    string producto;
+    int cantidad;
+    float precioTotal;
+};
+
+
+
+int main(){
+
+    return 0;
+}
